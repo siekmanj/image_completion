1319f2fde98c5e92cbd12cd9bfa8023357355c6e
siekmanj <siekmanj@oregonstate.edu>
2020-12-04 10:29:28 -0800

    initial commit

---
+++ complete.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cvxpy as cp
+import random
+
+def complete_matrix(M, omega):
+    # create a decision variable which has shape 2n X 2n
+    # essentially,
+    # | W1 X  |
+    # | X* W2 |
+    # we want to minimize Tr(W1) + Tr(W2), but only really care
+    # about X as this will be our original reconstructed matrix.
+    # The reason for introducing W1, W2, and X* is because for 
+    # convex optimization reasons, we want to minimize the trace of
+    # a symmetric PSD matrix.
+    X = cp.Variable([np.sum(M.shape), np.sum(M.shape)], PSD=True) # create the 2n X 2n matrix
+
+    # Constraint explanation
+    constraints = [X == X.T] # symmetry constraint
+    for i, j in omega:
+        constraints += [X[i, j + M.shape[0]] == M[i, j]]
+
+    # Minimize surrogate of nuclear norm, trace
+    problem = cp.Problem(cp.Minimize(cp.trace(X)), constraints)
+    problem.solve()
+
+    # return bottom left corner of matrix
+    return X.value[:M.shape[0], M.shape[0]:]
+
+def mask_out_matrix(X, entries):
+    mask = np.zeros(X.shape)
+    omega = random.sample([(i, j) for i in range(X.shape[0]) for j in range(X.shape[1])], entries)
+    for (i, j) in omega:
+        mask[i, j] = 1
+    return X.copy() * mask, omega
+
+
+if __name__ == '__main__':
+    n    = 30
+    rank = 8
+    m    = int(0.4 * n**(1.25) * rank * np.log(n))
+    print(m)
+    M    = np.dot(np.random.randn(n, rank), np.random.randn(n, rank).T)
+    print(np.linalg.matrix_rank(M))
+    X, omega = mask_out_matrix(M, m)
+        
+    #max_val = np.max(np.absolute(M))
+
+    print("Nuclear Norm of original matrix M: {:6.4f}".format(np.linalg.norm(M, "nuc")))
+
+    #X = M.copy() * mask
+
+    print('Average entry-wise difference before recovery:', np.mean(np.abs(M - X)))
+
+    recovered = complete_matrix(X, omega)
+
+    print('Average entry-wise difference after recovery:', np.mean(np.abs(M - recovered)))
+    print("Nuclear Norm of recoverd matrix: {:6.4f}".format(np.linalg.norm(recovered, "nuc")))
+    
+
---
+++ mnist_demo.py
@@ -0,0 +1,70 @@
+import numpy as np
+import random
+
+from complete import complete_matrix, mask_out_matrix
+from mnist import MNIST
+
+def mask_out_nonzero(X, proportion):
+    """
+    Mask out only nonzero entries.
+    """
+    px_to_remove = proportion * len(X.flatten())
+
+    mask      = np.zeros(X.shape)
+    anti_mask = np.zeros(X.shape)
+
+    entries = [(i, j) for i in range(X.shape[0]) for j in range(X.shape[1])]
+
+    np.random.shuffle(entries)
+
+    omega      = nonzero_entries[:int(proportion*len(nonzero_entries))] # entries which stay the same (input to optimization algo)
+    anti_omega = nonzero_entries[int(proportion*len(nonzero_entries)):] # entries which are modified
+
+    for (i, j) in omega:
+        mask[i, j] = 1
+
+    for (i, j) in anti_omega:
+        anti_mask[i, j] = 1
+
+    #return X.copy() * mask + null_matrix, omega
+    return mask, anti_mask, omega
+
+if __name__ == '__main__':
+
+    debug = True
+    mndata = MNIST('./mnist')
+    mndata.gz = True
+    images, labels = mndata.load_training()
+
+    np.random.seed(0)
+
+    batch = list(range(len(images)))
+    np.random.shuffle(batch)
+    batch = batch[:100]
+
+    print(batch)
+
+    for i, (img, label) in enumerate(zip([images[i] for i in batch], [labels[i] for i in batch])): 
+        M                      = np.reshape(img, (28,28))
+        mask, anti_mask, omega = remove_entries(M, 0.1)
+        if debug:
+          print(np.max(M))
+          print("Mask:")
+          #print((255*mask).flatten())
+          print(MNIST.display(255*(anti_mask).flatten()))
+          print(MNIST.display(255*(mask).flatten()))
+          print("^^MASK")
+
+        recovered     = complete_matrix(mask*M, omega)
+
+        rank_m         = np.linalg.matrix_rank(M)
+        err_frobenius  = np.linalg.norm(M - recovered, 'fro')
+        frobenius_diff = np.linalg.norm(M, 'nuc') - np.linalg.norm(recovered, 'nuc')
+
+        print("Original matrix rank: {:2d} | Frobenius Norm of Diff: {:4.3f} | Nuclear Norm Diff {:6.3f}".format(rank_m, err_frobenius, frobenius_diff))
+
+        if debug:
+          pass
+          #print(MNIST.display(img))
+          #print(MNIST.display(masked.flatten()))
+          #print(MNIST.display(recovered.flatten()))
